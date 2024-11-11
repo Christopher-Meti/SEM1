2b28c05ca4626138790b0aff390f916fdf83210e
Christopher Metnitzer <s55652@edu.campus02.at>
2024-11-11 14:08:26 +0100

    layout

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 liste = [[1,"iPhone", 500],[2, "Bier", 5],[3, "MacBook", 2000],[4, "Sofa", 1000],[5, "Zigaretten", 7]]
 bestellung = [[1,1], [1,1], [1,1], [1,1], [3,1], [3,1]]
+
+def rabatt():
+    pass
+
+def steuern():
+    pass
+
+def bestellsumme():
+    pass
